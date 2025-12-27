0859a98c5c2feed1c85f89e1c382c5a60368f29f
thonexdp <pazjeck14@gmail.com>
2025-12-27 17:35:44 +0800

    fix error

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -7,7 +7,7 @@
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
     @routes
     @viteReactRefresh
-    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+    @vite(['resources/js/app.jsx'])
     @inertiaHead
 </head>
 
